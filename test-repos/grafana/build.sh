3139c6a7554a78bf57c8b2b489f2afbc4a8f630c
kkx9 <64183990+kkx9@users.noreply.github.com>
2023-06-08 15:10:13 +0800

    build 20 commits

--- test-repos/grafana/build.sh
+++ test-repos/grafana/build.sh
@@ -1,7 +1,22 @@
+i=0
 while read line; do
   git reset --hard $line
   echo "commit:$line" >> /go/src/github.com/docker/docker/trans/buildcache.log
+  echo "commit:$line" >> /go/src/github.com/docker/docker/trans/buildtime.log
   docker build --build-arg HTTP_PROXY=http://202.114.7.81:7890 \
-	 --build-arg HTTPS_PROXY=http://202.114.7.81:7890 \
-	 -f Dockerfile -t grafana:$line ./
+	  --build-arg HTTPS_PROXY=http://202.114.7.81:7890 \
+	  -f Dockerfile -t grafana:$line ./
+  if [ $? -eq 0 ]
+  then
+	  i=$((i+1))
+  else
+	  echo "failed" >> /go/src/github.com/docker/docker/trans/buildcache.log
+	  echo "failed" >> /go/src/github.com/docker/docker/trans/buildtime.log
+  fi
+  if [ $i -eq 20 ]
+  then
+          break
+  else
+          continue
+  fi
 done < commits.txt
