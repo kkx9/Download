2c85acc31270fc5225ff3d2a4a9fdd636c037658
kkx9 <64183990+kkx9@users.noreply.github.com>
2023-06-13 16:50:22 +0800

    Update build.sh

--- test-repos/superset/build.sh
+++ test-repos/superset/build.sh
@@ -4,5 +4,5 @@ while read line; do
   echo "commit:$line" >> /go/src/github.com/docker/docker/trans/buildtime.log
   docker build --build-arg HTTP_PROXY=http://202.114.7.81:7890 \
           --build-arg HTTPS_PROXY=http://202.114.7.81:7890 \
-          -f Dockerfile -t jumpserver:$line ./
+          -f Dockerfile -t superset:$line ./
 done < commits20.txt
