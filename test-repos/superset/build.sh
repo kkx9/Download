5d33f5ccbe206d20ae267ae84fdc66ede1b7a8ff
kkx9 <64183990+kkx9@users.noreply.github.com>
2023-06-13 16:47:57 +0800

    Create build.sh

---
+++ test-repos/superset/build.sh
@@ -0,0 +1,8 @@
+while read line; do
+  git reset --hard $line
+  echo "commit:$line" >> /go/src/github.com/docker/docker/trans/buildcache.log
+  echo "commit:$line" >> /go/src/github.com/docker/docker/trans/buildtime.log
+  docker build --build-arg HTTP_PROXY=http://202.114.7.81:7890 \
+          --build-arg HTTPS_PROXY=http://202.114.7.81:7890 \
+          -f Dockerfile -t jumpserver:$line ./
+done < commits20.txt
